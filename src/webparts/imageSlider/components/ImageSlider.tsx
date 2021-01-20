723940eefa5c8b776382a31c34e7314cd7b65d20
Shawn Fagan <shawn@fagan.me>
2021-01-20 16:00:04 -0500

    Added Code Comments

--- src/webparts/imageSlider/components/ImageSlider.tsx
+++ src/webparts/imageSlider/components/ImageSlider.tsx
@@ -6,7 +6,7 @@ import { Slide } from "./Slide";
 import { StaticImage } from "./StaticImage";
 import { SliderImageItems } from "../models/SliderImageItems";
 import { ImageSliderService } from "../services/ImageSliderService";
-import * as moment from 'moment';
+import * as moment from "moment";
 export default class ImageSlider extends React.Component<
   IImageSliderProps,
   IImageSliderState
@@ -19,6 +19,7 @@ export default class ImageSlider extends React.Component<
     this.goToPrevSlide = this.goToPrevSlide.bind(this);
     this.goToNextSlide = this.goToNextSlide.bind(this);
     this.onclickNextSlide = this.onclickNextSlide.bind(this);
+    this.onclickPrevSlide = this.onclickPrevSlide.bind(this);
     this.startTimer = this.startTimer.bind(this);
     this.state = {
       slides: null,
@@ -28,15 +29,32 @@ export default class ImageSlider extends React.Component<
     };
   }
 
+  /**
+   * Handles action to go to a selected slide.
+   */
   private goToSlide = (index) => {
     this.setState({ activeIndex: index, running: false });
   }
 
+  /**
+   * Handles onclick to go to the next slide.
+   */
+  private onclickPrevSlide = (e) => {
+    e.preventDefault();
+    this.goToPrevSlide(e);
+  }
+
+  /**
+   * Handles onclick to go to the next slide.
+   */
   private onclickNextSlide = (e) => {
     e.preventDefault();
     this.goToNextSlide();
   }
 
+  /**
+   * Handles action to go to the pervious slide.
+   */
   private goToPrevSlide = (e) => {
     const { activeIndex, slides } = this.state;
     e.preventDefault();
@@ -52,6 +70,9 @@ export default class ImageSlider extends React.Component<
     });
   }
 
+  /**
+   * Handles action to go to the next slide.
+   */
   private goToNextSlide = () => {
     const { activeIndex, slides, speed } = this.state;
     let index = activeIndex;
@@ -68,21 +89,28 @@ export default class ImageSlider extends React.Component<
     });
   }
 
+  /**
+   * Function get the images from the SharePoint list.
+   */
   private getImages = () => {
     const { context, displayView } = this.props;
     ImageSliderService.GetItems(context, displayView)
-    .then((spItems: SliderImageItems[]) => {
-      this.setState({ slides: spItems });
-      if (spItems.length > 1) {
-        this.startTimer();
-      }
-    })
-    .catch((error: any) => {
-      console.error(error);
-    });
+      .then((spItems: SliderImageItems[]) => {
+        this.setState({ slides: spItems });
+        if (spItems.length > 1) {
+          this.startTimer();
+        }
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
   }
 
-  private startTimer = (newInterval?:number) => {
+  /**
+   * Handles the start of the slider timer.
+   * @param newInterval
+   */
+  private startTimer = (newInterval?: number) => {
     const { activeIndex, speed } = this.state;
     var interval: number = newInterval !== undefined ? newInterval : speed;
     this.timer = setInterval(() => {
@@ -94,27 +122,39 @@ export default class ImageSlider extends React.Component<
       }
     }, interval);
   }
-      
+
+  /**
+   * Handles component mount lifecycle method.
+   */
   public componentDidMount = () => {
+    //Get Sites from SharePoint Library on Web Part Mount
     this.getImages();
   }
 
+  /**
+   * Handles component update lifecycle method.
+   * @param prevProps
+   */
   public componentDidUpdate(prevProps: IImageSliderProps) {
     const currProps = this.props;
     if (prevProps.displayView !== currProps.displayView) {
       //clearInterval(this.timer);
       this.getImages();
     }
-    if (prevProps.slideSpeed !== currProps.slideSpeed){
+    if (prevProps.slideSpeed !== currProps.slideSpeed) {
       this.setState({
         activeIndex: 0,
-        speed: currProps.slideSpeed 
+        speed: currProps.slideSpeed,
       });
       //clearInterval(this.timer);
       this.startTimer(currProps.slideSpeed);
     }
   }
 
+  /**
+   * Handles component render.
+   * @param prevProps
+   */
   public render(): React.ReactElement<IImageSliderProps> {
     const { slides } = this.state;
     if (slides === null) {
